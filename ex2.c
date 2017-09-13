df5a7e34186f727063e7599da7460272adfb5778
Kaley19 <31711493+Kaley19@users.noreply.github.com>
2017-09-13 18:09:34 -0400

    delete comments

--- ex2.c
+++ ex2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void PrintResults(int counts[], int total)//Given output function
+void PrintResults(int counts[], int total)
 {
     int i;
     for(i = 0; i < 26; i++){

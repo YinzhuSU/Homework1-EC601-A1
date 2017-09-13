52fded2310b9871c2b725ddd21b7d8866fc28239
Kaley19 <31711493+Kaley19@users.noreply.github.com>
2017-09-13 18:09:59 -0400

    change variable name

--- ex2.c
+++ ex2.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void PrintResults(int counts[], int total)
+void PrintResults(int counts[], int Total)
 {
     int i;
     for(i = 0; i < 26; i++){
         printf("%c %d\n", i + 97, counts[i]);
     }
-    printf("Total count - %d\n", total);
+    printf("Total count - %d\n", Total);
 }
